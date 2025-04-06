ab8ac57ed4c0d237c867413e5af01d276004961d
CD1904 <cduffyhayes19@gmail.com>
2025-04-07 00:23:27 +0100

    Create Calculatoe.py

---
+++ Calculatoe.py
@@ -0,0 +1,9 @@
+length = float(input("Enter the lenght of a rectangle: "))
+width = float(input("Etner the width of a rectangle: "))
+height = float(input("Enter the hright of a rectamgle: "))
+
+area = length * width
+volume = length * width * height
+
+print (f"The area is: {area}cm^2")
+print (f"The volume is: {volume}cm^3")
